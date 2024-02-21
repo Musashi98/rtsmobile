f5709845c30b098ae765bcc360bc6d50b12f7fd7
Musashi98 <lsuarez.wrk@gmail.com>
2024-02-20 23:03:02 -0300

    Added needed packages and basic configuration

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TamaguiProvider } from 'tamagui';
+import tamaguiConfig from 'tamagui.config';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider config={tamaguiConfig}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TamaguiProvider>
   );
 }
 
---
+++ firebase.config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyA72TstT6WhInOHa0Ko0eGENCCPwgArHLo',
+    authDomain: 'rtsmobile-3d79e.firebaseapp.com',
+    projectId: 'rtsmobile-3d79e',
+    storageBucket: 'rtsmobile-3d79e.appspot.com',
+    messagingSenderId: '1055618198302',
+    appId: '1:1055618198302:web:65de80ae0a9556dffc370f',
+    measurementId: 'G-GSYRHHYR0H',
+};
+
+export default firebaseConfig
